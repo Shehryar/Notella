4b5f194159ef06630fc018de6952ab80ecdd64c1
Shehryar <ssh2690@gmail.com>
2013-03-13 20:11:38 -0400

    Initial Commit

---
+++ ControlTest/MasterViewController.h
@@ -0,0 +1,18 @@
+//
+//  MasterViewController.h
+//  ControlTest
+//
+//  Created by Shehryar Hussain on 3/13/13.
+//  Copyright (c) 2013 Shehryar Hussain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
---
+++ ControlTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ControlTest
+//
+//  Created by Shehryar Hussain on 3/13/13.
+//  Copyright (c) 2013 Shehryar Hussain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
