26e5eed176083fa43ee9ea699c7aafa68ce5304e
Shehryar <ssh2690@gmail.com>
2013-03-25 15:07:41 -0400

    Version 1.0
    Complete sans icon

---
+++ Pods/Pods-resources.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+install_resource()
+{
+  case $1 in
+    *.storyboard)
+      echo "ibtool --errors --warnings --notices --output-format human-readable-text --compile ${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename \"$1\" .storyboard`.storyboardc ${PODS_ROOT}/$1 --sdk ${SDKROOT}"
+      ibtool --errors --warnings --notices --output-format human-readable-text --compile "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename \"$1\" .storyboard`.storyboardc" "${PODS_ROOT}/$1" --sdk "${SDKROOT}"
+      ;;
+    *.xib)
+        echo "ibtool --errors --warnings --notices --output-format human-readable-text --compile ${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename \"$1\" .xib`.nib ${PODS_ROOT}/$1 --sdk ${SDKROOT}"
+      ibtool --errors --warnings --notices --output-format human-readable-text --compile "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename \"$1\" .xib`.nib" "${PODS_ROOT}/$1" --sdk "${SDKROOT}"
+      ;;
+    *.framework)
+      echo "rsync -rp ${PODS_ROOT}/$1 ${CONFIGURATION_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}"
+      rsync -rp "${PODS_ROOT}/$1" "${CONFIGURATION_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}"
+      ;;
+    *.xcdatamodeld)
+      echo "xcrun momc ${PODS_ROOT}/$1 ${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename $1 .xcdatamodeld`.momd"
+      xcrun momc "${PODS_ROOT}/$1" "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename $1 .xcdatamodeld`.momd"
+      ;;
+    *)
+      echo "cp -R ${PODS_ROOT}/$1 ${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
+      cp -R "${PODS_ROOT}/$1" "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
+      ;;
+  esac
+}
+install_resource 'NUI/NUI/Resources/NUIViewBackground.png'
+install_resource 'NUI/NUI/NUIStyle.nss'
+install_resource 'NUI/NUI/Themes/Blue.NUI.nss'
+install_resource 'NUI/NUI/Themes/Default.NUI.nss'
+install_resource 'NUI/NUI/Themes/Googolplex.NUI.nss'
+install_resource 'NUI/NUI/Themes/Round.NUI.nss'
+install_resource 'NUI/NUI/Themes/Route.NUI.nss'
+install_resource 'NUI/NUI/Themes/SkyBlue.NUI.nss'
+install_resource 'NUI/NUI/Themes/Switchboard.NUI.nss'
