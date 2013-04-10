a184426330b0e85be22062fc0c34531f58eded06
Shehryar <ssh2690@gmail.com>
2013-04-10 12:40:53 -0400

    Fixed sizing for screenshots

--- ControlTest/ScreenShotViewController.h
+++ ControlTest/ScreenShotViewController.h
@@ -8,6 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ScreenShotViewController : UIViewController
+@interface ScreenShotViewController : UIViewController <UIScrollViewDelegate>
+
+@property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (strong, nonatomic) IBOutlet UIPageControl *pageControl;
+- (IBAction)dimiss:(id)sender;
+@property (strong, nonatomic) IBOutlet UIBarButtonItem *backBarButton;
 
 @end
