fc8907e7705f190c0fef23c3f56bdab1b3b9f519
Shehryar <ssh2690@gmail.com>
2013-04-10 12:40:18 -0400

    Fixed textview scrolling issue.
    Along with that fixed screenshot sizing for Retina devices on the help
    screen. Fixed the iPad compatability mode issues.

---
+++ ControlTest/ScreenShotViewController.h
@@ -0,0 +1,13 @@
+//
+//  ScreenShotViewController.h
+//  Notella
+//
+//  Created by Shehryar Hussain on 3/27/13.
+//  Copyright (c) 2013 Shehryar Hussain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScreenShotViewController : UIViewController
+
+@end
---
+++ ControlTest/ScreenShotViewController.m
@@ -0,0 +1,38 @@
+//
+//  ScreenShotViewController.m
+//  Notella
+//
+//  Created by Shehryar Hussain on 3/27/13.
+//  Copyright (c) 2013 Shehryar Hussain. All rights reserved.
+//
+
+#import "ScreenShotViewController.h"
+
+@interface ScreenShotViewController ()
+
+@end
+
+@implementation ScreenShotViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
